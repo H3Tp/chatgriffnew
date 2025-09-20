f0895831b83641bf32b2c23f89bfe7d6c4785fc8
H3Tp <111122338+H3Tp@users.noreply.github.com>
2025-09-20 00:47:50 -0400

    fix(router): correct routes + imports for all pages

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
